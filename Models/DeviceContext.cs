596094f6f7ced2b31d0ea99837cc5994d2968c03
JasonBoothSWE <73965452+JasonBoothSWE@users.noreply.github.com>
2023-01-12 13:20:55 -0600

    setup device controller and context

---
+++ Models/DeviceContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeviceManager.Models
+{
+    public class DeviceContext : DbContext
+    {
+        public DeviceContext(DbContextOptions<DeviceContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Device> Devices { get; set; } = null!;
+    }
+}
