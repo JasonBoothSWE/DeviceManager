9e1bb38feac1fcbfe306426befcf2c2b78eee316
JasonBoothSWE <73965452+JasonBoothSWE@users.noreply.github.com>
2023-01-12 11:55:03 -0600

    Add project files.

---
+++ Models/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace DeviceManager.Models
+{
+    public class Device
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        
+        public bool Status { get; set; }
+    }
+}
